d2b681bf90102e9ed9473eb920a9a35994710f71
Your Name <you@example.com>
2024-01-08 14:59:20 +0800

    Released v0.0.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 
-VERSION = '$VERSION'
+VERSION = '`$VERSION`'
 
 setup(
     name='cc-feishu',  # package name
